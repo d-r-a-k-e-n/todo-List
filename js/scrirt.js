78f79278410f191088d8093a4251fbdfecdf900a
d-r-a-k-e-n <150184648+d-r-a-k-e-n@users.noreply.github.com>
2024-06-11 23:25:01 +0300

    Delete js directory

--- js/scrirt.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector(".backdrop");
-const modalBtnOpen = document.querySelectorAll(".modal-btn-open");
-const modalBtnClose = document.querySelectorAll(".modal-btn-close");
-
-const toggleModal = () => modal.classList.toggle("is-hidden");
-
-modalBtnOpen.addEventListener("click", toggleModal);
-modalBtnClose.addEventListener("click", toggleModal);
